e983bda12ab767410d576daf782d56884f81cf6d
Naveed Jamali <naveedjamali@outlook.com>
2022-06-14 12:58:28 +0500

    commit

--- app/src/main/java/com/example/checkboxapp/MainActivity.java
+++ app/src/main/java/com/example/checkboxapp/MainActivity.java
@@ -12,6 +12,9 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * Declare UI elements
+     */
     CheckBox checkBox1, checkBox2, checkBox3;
     Button selectButton;
     TextView label;
@@ -20,18 +23,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /**
+         * Link Java UI elements with XML UI elements using each one's ID
+         */
         checkBox1 = (CheckBox) findViewById(R.id.checkBox);
         checkBox2 = (CheckBox) findViewById(R.id.checkBox2);
         checkBox3 = (CheckBox) findViewById(R.id.checkBox3);
         selectButton = (Button) findViewById(R.id.selectBtn);
         label = (TextView) findViewById(R.id.label);
 
+        /**
+         * Set a click listener on the button to do some task when click
+         */
         selectButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                // String variable to save text of checked checkboxes.
                 String text = "";
 
+                // examine that if the checkbox is checked, then concatenate its text in String text variable.
                 if (checkBox1.isChecked()) {
                     text += checkBox1.getText();
                 }
@@ -42,7 +52,11 @@ public class MainActivity extends AppCompatActivity {
                     text += " " + checkBox3.getText();
                 }
 
+                /** after examining all the checkboxes,
+                 *  Display the String text variable's content on label UI element.
+                 */
                 label.setText(text);
+
             }
         });
 
